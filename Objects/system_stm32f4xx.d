5d4faeda63957d9ab0e9aa92e4be666eb4d7c25a
annikola <annikolaprof@gmail.com>
2025-04-26 18:25:57 +0300

    Initial commit

---
+++ Objects/system_stm32f4xx.d
@@ -0,0 +1,5 @@
+./objects/system_stm32f4xx.o: RTE\Device\STM32F411RETx\system_stm32f4xx.c \
+  C:\Users\30690\AppData\Local\arm\packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f4xx.h \
+  C:\Users\30690\AppData\Local\arm\packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f411xe.h \
+  C:\Users\30690\AppData\Local\arm\packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Users\30690\AppData\Local\arm\packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
